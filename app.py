ac551408f1ed1bc2fc5d848340bd5457acb0e97c
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2019-08-31 23:29:11 +0100

    Initial Commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect, request, url_for
+# import os
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+import env as config
+
+
+app = Flask(__name__)
+
+
+app.config['MONGO_DBNAME'] = 'chef_mateDB'
+app.config['MONGO_URI'] = config.MONGO_URI
+
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("index.html",
+                           recipes=mongo.db.recipes.find())
+
+
+if __name__ == '__main__':
+    # app.run(host=os.environ.get('IP'),
+    #          port=int(os.environ.get('PORT')))
+    app.run(debug=True)
