ebfe33f3d603d65cac354f5f1935c2bc2ee2d039
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2019-09-07 11:19:17 +0100

    add receipes page done

--- app.py
+++ app.py
@@ -8,32 +8,63 @@ import env as config
 app = Flask(__name__)
 
 # configuration of Database
-app.config['MONGO_DBNAME'] = 'task_manager'
+app.config['MONGO_DBNAME'] = 'chef_mateDB'
 app.config['MONGO_URI'] = config.MONGO_URI
 app.config['SECRET_KEY'] = config.SECRET_KEY
 
 mongo = PyMongo(app)
 
 # routing for pages
+'''
+home page
+'''
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+'''
+receipes
+'''
+# get all recipes
 @app.route('/recipes')
 def recipes():
-    return render_template("recipes.html",
-                           recipes=mongo.db.recipes.find())
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
+# get a single recipe
+@app.route('/recipe/<recipe_id>')
+def recipe(recipe_id):
+    return render_template("recipe.html", recipe=mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)}))
+
+# add recipe
+@app.route('/add_recipe')
+def add_recipe():
+    the_diet = mongo.db.diets.find()
+    the_cuisine = mongo.db.cuisine.find()
+    return render_template('addrecipe.html',
+                           diets=the_diet, cuisine=the_cuisine)
+
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    tasks =  mongo.db.recipes
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('recipes'))
+
+
+
+"""
+Users / Log-in / Register
+"""
+# Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('index'))
-    return render_template('register.html', title='Register', form=form)
+  form = RegistrationForm()
+  if form.validate_on_submit():
+      flash(f'Account created for {form.username.data}!', 'success')
+      return redirect(url_for('index'))
+  return render_template('register.html', title='Register', form=form)
 
-
+# Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   form = LoginForm()
