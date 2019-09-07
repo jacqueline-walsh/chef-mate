bc0b70717d75161dab7cb0d2e22c13f54d9bc7d6
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2019-09-07 18:14:09 +0100

    crud implementation for diets and cuisine

--- app.py
+++ app.py
@@ -22,6 +22,32 @@ home page
 def index():
     return render_template('index.html')
 
+
+"""
+Users / Log-in / Register
+"""
+# Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+      flash(f'Account created for {form.username.data}!', 'success')
+      return redirect(url_for('index'))
+  return render_template('register.html', title='Register', form=form)
+
+# Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    if form.username.data == 'jacqann' and form.password.data == 'password':
+      flash('You have been logged in!', 'success')
+      return redirect(url_for('index'))
+    else:
+      flash('login Unsuccessful.  Please check username and password!', 'danger')
+  return render_template('login.html', title='Login', form=form)
+
+
 '''
 receipes
 '''
@@ -51,30 +77,86 @@ def insert_recipe():
     return redirect(url_for('recipes'))
 
 
+'''
+Diets
+'''
 
-"""
-Users / Log-in / Register
-"""
-# Register
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-  form = RegistrationForm()
-  if form.validate_on_submit():
-      flash(f'Account created for {form.username.data}!', 'success')
-      return redirect(url_for('index'))
-  return render_template('register.html', title='Register', form=form)
+@app.route('/diets')
+def diets():
+    return render_template('diets.html', diets=mongo.db.diets.find())
 
-# Login
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-  form = LoginForm()
-  if form.validate_on_submit():
-    if form.username.data == 'jacqann' and form.password.data == 'password':
-      flash('You have been logged in!', 'success')
-      return redirect(url_for('index'))
-    else:
-      flash('login Unsuccessful.  Please check username and password!', 'danger')
-  return render_template('login.html', title='Login', form=form)
+@app.route('/add_diet')
+def add_diet():
+    return render_template('adddiet.html')
+
+
+@app.route('/insert_diet', methods=['POST'])
+def insert_diet():
+    diets = mongo.db.diets
+    diet_doc = {'diet_name': request.form.get('diet_name')}
+    diets.insert_one(diet_doc)
+    return redirect(url_for('diets'))
+
+
+@app.route('/edit_diet/<diet_id>')
+def edit_diet(diet_id):
+    return render_template('editdiet.html', diet=mongo.db.diets.find_one(
+    {'_id': ObjectId(diet_id)}))
+
+
+@app.route('/update_diet/<diet_id>', methods=['POST'])
+def update_diet(diet_id):
+    mongo.db.diets.replace_one(
+        {'_id': ObjectId(diet_id)},
+        {'diet_name': request.form.get('diet_name')})
+    return redirect(url_for('diets'))
+
+
+@app.route('/delete_diet/<diet_id>')
+def delete_diet(diet_id):
+    mongo.db.diets.delete_one({"_id": ObjectId(diet_id)})
+    return redirect(url_for('diets'))
+
+
+'''
+Cuisine
+'''
+
+@app.route('/cuisine')
+def cuisine():
+    return render_template('cuisine.html', cuisine=mongo.db.cuisine.find())
+
+@app.route('/add_cuisine')
+def add_cuisine():
+    return render_template('addcuisine.html')
+
+
+@app.route('/insert_cuisine', methods=['POST'])
+def insert_cuisine():
+    cuisine = mongo.db.cuisine
+    cuisine_doc = {'cuisine_country': request.form.get('cuisine_country')}
+    cuisine.insert_one(cuisine_doc)
+    return redirect(url_for('cuisine'))
+
+
+@app.route('/edit_cuisine/<cuisine_id>')
+def edit_cuisine(cuisine_id):
+    return render_template('editcuisine.html', cuisine=mongo.db.cuisine.find_one(
+    {'_id': ObjectId(cuisine_id)}))
+
+
+@app.route('/update_cuisine/<cuisine_id>', methods=['POST'])
+def update_cuisine(cuisine_id):
+    mongo.db.cuisine.replace_one(
+        {'_id': ObjectId(cuisine_id)},
+        {'cuisine_country': request.form.get('cuisine_country')})
+    return redirect(url_for('cuisine'))
+
+
+@app.route('/delete_cuisine/<cuisine_id>')
+def delete_cuisine(cuisine_id):
+    mongo.db.cuisine.delete_one({"_id": ObjectId(cuisine_id)})
+    return redirect(url_for('cuisine'))
 
 
 if __name__ == '__main__':
