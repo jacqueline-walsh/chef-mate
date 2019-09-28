fdb4fe08bbf3e853c1ff64af51b6e7f81d795f84
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2019-09-28 14:31:42 +0100

    config for heroku

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import os
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 from flask_bcrypt import Bcrypt
-import env as config
+
 
 app = Flask(__name__)
 
 # configuration of Database
-app.config['MONGO_URI'] = config.MONGO_URI
-app.config['SECRET_KEY'] = config.SECRET_KEY
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 
 bcrypt = Bcrypt(app)
@@ -377,6 +377,5 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=os.environ.get('PORT'), 
-            debug=True)
+    app.run(host=os.environ.get('IP', "0.0.0.0"),
+            port=os.environ.get('PORT', "5000"))
